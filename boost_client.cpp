56dd4d6436d4df7db97e3231067effc44c9dbaeb
Yuya Ochiai <yuya-oc@is.naist.jp>
2012-10-13 15:06:39 +0900

    add boost_system

---
+++ boost_client.cpp
@@ -0,0 +1,10 @@
+/*
+ * boost_client.cpp
+ *
+ *  Created on: 2012/10/13
+ *      Author: yuya-oc
+ */
+
+int main(int argc, char** argv){
+	return 0;
+}
---
+++ boost_server.cpp
@@ -0,0 +1,12 @@
+/*
+ * boost_server.cpp
+ *
+ *  Created on: 2012/10/13
+ *      Author: yuya-oc
+ */
+
+#include <boost/asio.hpp>
+
+int main(int argc, char** argv){
+	return 0;
+}
