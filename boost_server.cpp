6de5a3de42b84bd4634807b201352b94f4878bb7
Yuya Ochiai <yuya-oc@is.naist.jp>
2012-10-13 15:15:50 +0900

    implement boost_server

--- boost_server.cpp
+++ boost_server.cpp
@@ -5,8 +5,25 @@
  *      Author: yuya-oc
  */
 
+#include <iostream>
 #include <boost/asio.hpp>
 
+namespace asio = boost::asio;
+using namespace asio::ip;
+
 int main(int argc, char** argv){
+	asio::io_service io_service;
+	tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(),12345));
+	tcp::socket socket(io_service);
+
+	boost::system::error_code error;
+	acceptor.accept(socket,error);
+
+	if(error){
+		std::cerr << "connect failed:" << error.message() << std::endl;
+	}
+	else{
+		std::cout << "connected" << std::endl;
+	}
 	return 0;
 }
