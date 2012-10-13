ac30ace7f51b19c1586d043f8926d90e3e5d93f0
Yuya Ochiai <yuya-oc@is.naist.jp>
2012-10-13 14:58:21 +0900

    tmp implement

--- client.cpp
+++ client.cpp
@@ -5,5 +5,10 @@
 #include <sys/types.h>
 
 int main(int argc, char** argv){
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_fd < 0){
+		cerr << "Socket open failed" << endl;
+		exit(1);
+	}
 	return 0;
 }
