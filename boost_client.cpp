fd968d2f1308b5634aefab437546734f8dcf377d
Yuya Ochiai <yuya-oc@is.naist.jp>
2012-10-13 15:16:02 +0900

    implement boost_client

--- boost_client.cpp
+++ boost_client.cpp
@@ -5,6 +5,24 @@
  *      Author: yuya-oc
  */
 
+#include <iostream>
+#include <boost/asio.hpp>
+
+namespace asio = boost::asio;
+using namespace asio::ip;
+
 int main(int argc, char** argv){
+	asio::io_service io_service;
+	tcp::socket socket(io_service);
+
+	boost::system::error_code error;
+	socket.connect(tcp::endpoint(asio::ip::address::from_string("127.0.0.1"),12345),error);
+
+	if(error){
+		std::cerr << "connect failed:" << error.message() << std::endl;
+	}
+	else{
+		std::cout << "connected" << std::endl;
+	}
 	return 0;
 }
